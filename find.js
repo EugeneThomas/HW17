f7cc4b2b21b192b954375adc9e8199a9a4a3a708
EugeneThomas <ethomas1@stuy.edu>
2017-12-12 13:18:55 -0500

    Added to the Skeleton:

--- find.js
+++ find.js
@@ -14,17 +14,27 @@ console.log( "box width: " + boxWidth );
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
   /* YOUR IMPLEMENTATION */
+  return Math.sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1));
 };
 
 
 var findIt = function(e) {
   /* YOUR IMPLEMENTATION */
+  if (distance(e.X, e.Y, targetX, targetY) < 5) {
+    // Do something good
+  }
+  else {
+    // Convert the box to a specific color
+  }
 };
 
 /*
 your OTHER FXNS
-
 */
 
-box.addEventListener("mousemove", findIt);
+var colorScheme = function(e) {
+  // Use the distance to change the color
+  // Use backgroundcolor inside of the body tag. 
+}
 
+box.addEventListener("mousemove", findIt);
